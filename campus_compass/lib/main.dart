c0542ae20564d6d14d1d66fb531b6810799d299f
Khashayar Zardoui <161512870+Khashi99@users.noreply.github.com>
2026-03-23 17:08:11 -0400

    backend update with flutter wiring

--- campus_compass/lib/main.dart
+++ campus_compass/lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:campus_compass/firebase_options.dart';
 import 'package:campus_compass/screens/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:campus_compass/theme/app_colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  if (FirebaseAuth.instance.currentUser == null) {
+    await FirebaseAuth.instance.signInAnonymously();
+  }
+
   runApp(const MyApp());
 }
 
