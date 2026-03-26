68cafc783bb0b2c591c1b9fb4aef6718922f6257
Khashayar Zardoui <161512870+Khashi99@users.noreply.github.com>
2026-03-25 22:42:21 -0400

    added changes from the test branch

---
+++ campus_compass/lib/utils/video_player_controller_factory.dart
@@ -0,0 +1,3 @@
+export 'video_player_controller_factory_stub.dart'
+    if (dart.library.io) 'video_player_controller_factory_io.dart'
+    if (dart.library.html) 'video_player_controller_factory_web.dart';
