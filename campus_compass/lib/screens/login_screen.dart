52ef8f883501b3069eec7ea55dcdfb354b64b297
Salma Taha <salmetaha@gmail.com>
2026-04-05 23:23:20 -0400

    Removed University from Email textfield label

--- campus_compass/lib/screens/login_screen.dart
+++ campus_compass/lib/screens/login_screen.dart
@@ -75,7 +75,7 @@ class _MyWidgetState extends State<LoginScreen> {
                           child: Row(
                             children: [
                               Text(
-                                'University Email',
+                                'Email',
                                 style: AppTheme.bulletStyle,
                               )
                             ],
